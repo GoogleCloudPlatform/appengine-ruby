4308f6d99c9e83e4fa4393432428c66ae4b3fade
Daniel Azuma <dazuma@google.com>
2025-01-13 20:24:03 +0000

    chore(release): Switch to Ruby-based release reporter

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -7,9 +7,5 @@ set -eo pipefail
 export GEM_HOME=$HOME/.gem
 export PATH=$GEM_HOME/bin:$PATH
 
-python3 -m pip install git+https://github.com/googleapis/releasetool
-python3 -m pip install gcp-docuploader
 gem install --no-document toys
-
-python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
-toys release perform -v --enable-docs < /dev/null
+toys release perform -v --reporter-org=googlecloudplatform --enable-docs < /dev/null
